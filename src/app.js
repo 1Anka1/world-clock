3484052e6e08b88bba8d890043b80a3b71ecfb6c
Anna Kasianenko <97912793+1Anka1@users.noreply.github.com>
2023-07-30 13:48:29 +0200

    Add moment.js timezone

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function updateTime() {
-  odessaTimeElem.innerHTML = moment().format("LTS");
-  lvivTimeElem.innerHTML = moment().format("LTS");
-  charkowTimeElem.innerHTML = moment().format("LTS");
+  odessaTimeElem.innerHTML = moment().tz("Europe/Kiev").format("LTS");
+  lvivTimeElem.innerHTML = moment().tz("Europe/Kiev").format("LTS");
+  charkowTimeElem.innerHTML = moment().tz("Europe/Kiev").format("LTS");
 }
 setInterval(updateTime, 1);
 
@@ -9,7 +9,7 @@ setInterval(updateTime, 1);
 let odessaCityElem = document.querySelector("#odessa");
 let odessaDateElem = odessaCityElem.querySelector(".date");
 let odessaTimeElem = odessaCityElem.querySelector(".time");
-odessaDateElem.innerHTML = moment().format("MMM Do YYYY");
+odessaDateElem.innerHTML = moment().tz("Europe/Kiev").format("MMM Do YYYY");
 
 //lviv city
 
@@ -17,7 +17,7 @@ let lvivCityElem = document.querySelector("#lviv");
 let lvivDateElem = lvivCityElem.querySelector(".date");
 let lvivTimeElem = lvivCityElem.querySelector(".time");
 
-lvivDateElem.innerHTML = moment().format("MMM Do YYYY");
+lvivDateElem.innerHTML = moment().tz("Europe/Kiev").format("MMM Do YYYY");
 
 //charkow city
 
@@ -25,7 +25,7 @@ let charkowCityElem = document.querySelector("#charkow");
 let charkowDateElem = charkowCityElem.querySelector(".date");
 let charkowTimeElem = charkowCityElem.querySelector(".time");
 
-charkowDateElem.innerHTML = moment().format("MMM Do YYYY");
+charkowDateElem.innerHTML = moment().tz("Europe/Kiev").format("MMM Do YYYY");
 
 let today = moment().format("dddd Do MMMM");
 let todayElement = document.querySelector("#today");
