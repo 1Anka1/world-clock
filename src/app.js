ff42902bca32cca23d6b6cd85d6481a493417874
Anna Kasianenko <97912793+1Anka1@users.noreply.github.com>
2023-08-06 14:05:20 +0200

    Change style

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
 let selectCityElement = document.querySelector("#select-city");
 let cityElem = document.querySelector(".city-container");
-let citiesElem = document.querySelector(".cities");
 let cityTimeElem = document.querySelector(".time");
+
 selectCityElement.addEventListener("change", showDate);
 
 let timer;
+let isTimeHidden = false;
 
 function showDate(event) {
   let cityTimeZone = event.target.value;
@@ -17,13 +18,29 @@ function showDate(event) {
     cityTimeZone = moment.tz.guess();
   }
 
-  showTime(cityTimeZone);
+  const cityElemContent =
+    cityTimeZone.length === 0
+      ? `<h2 class="select-title">Please select a city</h2>`
+      : `<h2 class="city-name">${cityTimeZone}</h2>
+      <div class="date">${moment()
+        .tz(cityTimeZone)
+        .format("MMM Do YYYY")}</div>`;
+  cityElem.innerHTML = cityElemContent;
 
-  let currentDate = moment().tz(cityTimeZone).format("MMM Do YYYY");
-  cityElem.innerHTML = `<h2 class="city-name">${cityTimeZone}</h2>
-        <div class="date">${currentDate}</div>`;
+  if (cityTimeZone.length === 0) {
+    if (!isTimeHidden) {
+      cityTimeElem.style.display = "none"; // Hide the time element
+      isTimeHidden = true;
+    }
+  } else {
+    showTime(cityTimeZone);
+
+    if (isTimeHidden) {
+      cityTimeElem.style.display = "block"; // Display the time element
+      isTimeHidden = false;
+    }
+  }
 }
-
 function showTime(cityTimeZone) {
   clearInterval(timer);
   timer = setInterval(() => {
